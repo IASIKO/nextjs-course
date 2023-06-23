f1b8b06433bffe5bb3d413ec9d45f0ded6b4d792
IASIKO <iaseshviligi@gmail.com>
2023-06-23 19:30:26 +0400

    add

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "The first meetup",
+    image:
+      "https://www.lookandlearn.com/history-images/preview/A/A400/A400007.jpg",
+    address: "First meetup street 5, 12345 Somde City",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "The second meetup",
+    image:
+      "https://www.lookandlearn.com/history-images/preview/A/A400/A400007.jpg",
+    address: "Second meetup street 10, 12345 Somde City",
+    description: "This is a second meetup!",
+  },
+];
+
 function HomePage(params) {
-  return <h1>The Home Page</h1>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
