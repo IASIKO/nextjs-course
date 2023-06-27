fb592cc0b14a8e4301e2f827bc4eeecdbc348b6c
IASIKO <iaseshviligi@gmail.com>
2023-06-27 15:08:05 +0400

    add

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,14 +1,33 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetails(params) {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
-      image="https://www.lookandlearn.com/history-images/preview/A/A400/A400007.jpg"
-      title="satauri"
-      address="misamarti"
-      description="aghwera"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://www.lookandlearn.com/history-images/preview/A/A400/A400007.jpg",
+        title: "satauri",
+        address: "misamarti",
+        description: "aghwera",
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
