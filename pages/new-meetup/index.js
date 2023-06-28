e236f6b003f12470336b86564366c53fa09d2338
IASIKO <iaseshviligi@gmail.com>
2023-06-28 20:08:29 +0400

    add

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,25 +1,38 @@
 import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import { Fragment } from "react";
+import Head from "next/head";
 
 function NewMeetupPage() {
-  const router = useRouter()
+  const router = useRouter();
 
-    async function addMeetupHandler(meetup) {
-      const response = await fetch('/api/new-meetup', {
-        method: 'POST',
-        body: JSON.stringify(meetup),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
+  async function addMeetupHandler(meetup) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(meetup),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-      const data = await response.json()
-      console.log(data);
+    const data = await response.json();
+    console.log(data);
 
-      router.push('/')
-    }
+    router.push("/");
+  }
 
-  return <NewMeetupForm onAddMeetup={addMeetupHandler}/>;
+  return (
+    <Fragment>
+      <Head>
+        <title>Next.js new meetup</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups"
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Fragment>
+  );
 }
 
 export default NewMeetupPage;
