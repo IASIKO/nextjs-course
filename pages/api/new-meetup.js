f92c4282cd3fec3fd489eb1214b9dbafeb85d7f8
IASIKO <iaseshviligi@gmail.com>
2023-06-28 13:39:54 +0400

    add

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,25 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://iasiko:8888@cluster0.uxpuopp.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup iserted!" });
   }
 }
 
